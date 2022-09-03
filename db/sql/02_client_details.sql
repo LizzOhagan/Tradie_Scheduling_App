28c8a31865a7df6f93e6a23ea24d208316067cff
Elizabeth O'Hagan <elizabeth.ohagan@developersinstitute.co.nz>
2022-09-04 10:50:16 +1200

    -client details sql values created

---
+++ db/sql/02_client_details.sql
@@ -0,0 +1,11 @@
+INSERT INTO client_details (id, first_name, last_name, phone, address, email) VAULES
+(1, 'Han', 'Solo', '123456', '9 Millennium Falcon Ave, Georgetown, 5436', 'han@scoundrals.com'),
+(2, 'Sherlock', 'Holmes', '789456', '221B Baker Street, London, 5469', 'sholmes@dectives.com'),
+(3, 'Hermione', 'Granger', '145623', '8 Heathgate, Hampstead Garden Suburb, 5469', 'hgranger@ministryofmagic.com'),
+(4, 'James ', 'Bond', '489455', '30 Wellington Square, Chelsea, 5479', 'jb077@mi6.com'),
+(5, 'Clark ', 'Kent', '124879', '344 Clinton Street, Apartment 3D, Metroplis, 5479', 'super@man.com'),
+(6, 'Fitzwilliam', 'Darcy', '543395', 'Pemberley, Derbyshire, 5469', 'darcy@austin.com'),
+(7, 'Bruce', 'Wayne', '154133', '1007 Mountain Drive, Gotham, 5412', 'bw@bats.com'),
+(8, 'Dominic', 'Toretto', '125466', '1327 East Kensington Road, LA, 5436', 'dom@family.com'),
+(9, 'Robin', 'Hood', '236874', 'Sherwood Forest, Nottinghamshire, 5463', 'robin@merrymen.com'),
+(10, 'Robert', 'Balboa', '458996', '1818 East Tusculum Street, Kensington, 5496', 'rocky@eyeofthetiger.com'),
