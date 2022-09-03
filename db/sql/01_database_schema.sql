dabea7d4955665e39e3396db80c84057654bc88f
Elizabeth O'Hagan <elizabeth.ohagan@developersinstitute.co.nz>
2022-09-04 11:37:36 +1200

    database schema updated with correct commands. Spaces removed in client_details sql file

--- db/sql/01_database_schema.sql
+++ db/sql/01_database_schema.sql
@@ -1,9 +1,7 @@
-CREATE DATABASE keepTradingSchedule;
-
 CREATE TABLE client_details (
     id SERIAL PRIMARY KEY,
-    first_name text(255),
-    last_name text(255),
+    first_name varchar(255),
+    last_name varchar(255),
     phone varchar(50),
     address varchar(400),
     email varchar(100)
@@ -16,13 +14,11 @@ CREATE TABLE job_status (
     type status
 );
 
-
-
 CREATE TABLE job (
     id SERIAL PRIMARY KEY,
-    date_created datetime,
+    type TIMESTAMP,
     client_id int REFERENCES client_details(id),
     status_id int REFERENCES job_status(id),
-    quote money,
-    job_scope varchar
+    quote money default NULL,
+    job_scope varchar default NULL
 );
