b3dfad71c69ef0770e297e30df90eb87bae9f239
Elizabeth O'Hagan <elizabeth.ohagan@developersinstitute.co.nz>
2022-09-05 11:56:32 +1200

    deleted extra bracket in route and called the repo. Still not connecting to db

--- server/app.js
+++ server/app.js
@@ -2,19 +2,23 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const jobRouter = require("./jobs/job.router");
+const router = require("./jobs/job.router");
 const errorHandlerMiddleware = require("./middleware/errorHandlerMiddleware");
 
 //middleware
 app.use(cors());
 app.use(express.json());
 
+// app.use(() => {
+//   console.log("hellow");
+// });
+
 // routes
-app.use("/api/jobs", jobRouter); // route not yet working in insomnia
+app.use("/jobs", router); // route not yet working in insomnia
 
 // TODO:
-// app.use("api/jobs/:id")
-// app.use("api/jobs/:status")
+// app.use("/jobs/:id")
+// app.use("/jobs/:status")
 
 // error handling middleware
 app.use(errorHandlerMiddleware);
--- server/jobs/job.router.js
+++ server/jobs/job.router.js
@@ -3,14 +3,13 @@ const express = require("express");
 const router = express.Router();
 const getJobs = require("./job.repository");
 
-router.get("/"),
-  async (req, res, next) => {
-    try {
-      const jobs = await getJobs.getJobs;
-      return res.json(jobs);
-    } catch (err) {
-      next(err);
-    }
-  };
-
+router.get("/", async (req, res, next) => {
+  try {
+    const jobs = await getJobs.getJobs();
+    console.log(jobs);
+    return res.json(jobs);
+  } catch (err) {
+    next(err);
+  }
+});
 module.exports = router;
