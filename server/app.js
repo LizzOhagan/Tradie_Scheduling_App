5c574ef9cb974db0c9f22064f5473a42013ca8a4
Elizabeth O'Hagan <elizabeth.ohagan@developersinstitute.co.nz>
2022-09-05 00:11:52 +1200

    exported router, wrote scripts for job repo, router and errorhandlermiddlware

--- server/app.js
+++ server/app.js
@@ -2,13 +2,18 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const jobRouter = require("./jobs/job.router");
+// const errorHandlerMiddleware = require("./middleware/errorHandlerMiddleware");
 
 //middleware
 app.use(cors());
 app.use(express.json());
 
 // routes
+app.use("/api/jobs", jobRouter);
+// app.use("/jobs", jobRouter);
 
 // error handling middleware
+// app.use(errorHandlerMiddleware);
 
 module.exports = app;
---
+++ server/jobs/job.router.js
@@ -0,0 +1,16 @@
+const express = require("express");
+/* Creating a new router object. */
+const router = express.Router();
+const getJobs = require("./job.repository");
+
+router.get("/"),
+  async (req, res, next) => {
+    // try {
+    const jobs = await getJobs.getJobs;
+    return res.json(jobs);
+    // } catch (err) {
+    //   next(err);
+    // }
+  };
+
+module.exports = router;
