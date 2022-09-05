700aaf8330029cab6a337fa92effa4e89c254b77
Elizabeth O'Hagan <elizabeth.ohagan@developersinstitute.co.nz>
2022-09-05 20:22:42 +1200

    reinstalled yamljs in server foldergit add .

--- server/app.js
+++ server/app.js
@@ -4,6 +4,16 @@ const app = express();
 const cors = require("cors");
 const router = require("./jobs/job.router");
 const errorHandlerMiddleware = require("./middleware/errorHandlerMiddleware");
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs");
+const fs = require("fs");
+// const swaggerDocument = YAML.load("./apispec.yaml");
+
+// app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+const swaggerDocument = yaml.load(
+  fs.readFileSync(path.join(__dirname, "./apispec.yaml"), "utf8")
+);
 
 //middleware
 app.use(cors());
